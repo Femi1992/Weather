61c003a57da313b5f9cb26863a28b18d458f1464
Femi <olufemidokun@hotmail.co.uk>
2017-10-27 11:16:16 +0100

    Refactored

--- test.py
+++ test.py
@@ -4,58 +4,51 @@ import requests
 import json
 from geopy.geocoders import Nominatim
 
-geolocation = Nominatim()
-reverse = geolocation.reverse("51.51, -0.13")
-real_location = reverse
-print real_location.address
+class weather():
 
-client = MongoClient('localhost', 27017)
-db = client.weather
+        freegeoip = "http://freegeoip.net/json"
 
-collection = db.weather_collection
+        weather_url = 'http://api.openweathermap.org/data/2.5/weather?'
+        api_id = '6b35061c42fdbe1965095189659a796b'
+        q = ""
+        request_url = weather_url + 'appid=' + api_id + "&q="
+        json_object = ""
 
-post = {"City" : " ",
-        "Weather Condition" : " ",
-        "time" : ""}
+        def __init__(self):
+            print "Weather APP"
+            client = MongoClient('localhost', 27017)
+            db = client.weather
+            collection = db.weather_collection
 
+        def get_user_automatic(self):
+            with open('C:\\Users\\HP-PC\\Downloads\\city.list.json') as data_file:
+                data = json.load(data_file)
 
-print "****************Weather Forecast****************\n"
+                print data
 
-freegeoip = "http://freegeoip.net/json"
-geo_r = requests.get(freegeoip)
-geo_json = geo_r.json()
-
-user_latitude = geo_json['latitude']
-user_longitude = geo_json['longitude']
-
-
-#5day forecast
-#five_day_api = 'http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=6b35061c42fdbe1965095189659a796b'
-#url_1 = five_day_api + user_country
-"""
-while True:
-    location = raw_input("Location: ")
-
-    if location == "q":
-        break
-
-    main_api = ('http://api.openweathermap.org/data/2.5/weather?appid=6b35061c42fdbe1965095189659a796b&q=')
-    url = main_api + location
-    json_object = requests.get(url).json()
-    temp_kelvin = float( json_object['main']['temp'])
-    current_conditions = json_object['weather'][0]['description']
-    #cweather = json_object['weather']
-    temp_celsius = temp_kelvin - 273.15 #change from degrees kelvin to celsius
-
-    print "The current temperature in", location, "is", str(temp_celsius), "celsius and the current conditions are", current_conditions
+        def get_user_location(self):
+            print "Enter the current city you are in"
+            self.q = raw_input("Location: ")
+            self.request_url = self.request_url + self.q
 
 
-    #inserting data into db
-    now = datetime.datetime.now()
-    time_of_call =  now.strftime("%H:%M")
-    db.weather.insert_one({"City": location,
-                           "Weather Condition": current_conditions,
-                           "time": time_of_call
-                          })
-"""
 
+        def call_api(self):
+            self.json_object = requests.get(self.request_url).json()
+
+
+        def process_json(self):
+            temp_kelvin = float(self.json_object['main']['temp'])
+            current_conditions = self.json_object['weather'][0]['description']
+            temp_celsius = temp_kelvin - 273.15
+
+            print "The current temperature in", self.q, "is", str(temp_celsius), "celsius and the current conditions are", current_conditions
+
+
+
+if __name__ == "__main__":
+        w = weather()
+        w.get_user_automatic()
+        #w.get_user_location()
+        w.call_api()
+        w.process_json()
